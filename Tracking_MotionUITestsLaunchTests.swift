673135a65092d09dfabff260e69d4b94af37d19e
manisha1903 <manisha.kumari@kellton.com>
2026-06-19 12:36:16 +0530

    Add files via upload

---
+++ Tracking_MotionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tracking_MotionUITestsLaunchTests.swift
+//  Tracking_MotionUITests
+//
+//  Created by Manisha Kumari on 24/04/25.
+//
+
+import XCTest
+
+final class Tracking_MotionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
